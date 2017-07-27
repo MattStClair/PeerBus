477db7799a4e5541e9dfb4b7725d7af43f9e75e0
mackoyokcam <mark.hangarter@gmail.com>
2017-07-27 14:31:19 -0700

    added order constructor and storage

--- app.js
+++ app.js
@@ -12,23 +12,36 @@ for(var i = 0; i < names.length; i++) {
   dropdown.appendChild(option);
 }
 
+Order.all = [];
+
+function Order(item, quantity, name, street, zip, phoneNumber, creditCardNumber) {
+  this.item = item;
+  this.path = item + '.jpg';
+  this.quantity = quantity;
+  this.name = name;
+  this.street = street;
+  this.zip = zip;
+  this.phoneNumber = phoneNumber;
+  this.creditCardNumber = creditCardNumber;
+  Order.all.push(this);
+}
 
 
 function handleSubmit(e) {
   e.preventDefault();
 
   var item = e.target.dropdown.options[e.target.dropdown.options.selectedIndex].value;
+  var quantity = e.target.quantity.value;
+  var name = e.target.name.value;
+  var street = e.target.street.value;
+  var zip = e.target.zip.value;
+  var phoneNumber = e.target.phone_number.value;
+  var creditCardNumber = e.target.credit_card_number.value;
 
-  // var quantity = e.target.quantity.value;
-  // var name = e.target.name.value;
-  // var street = e.target.street.value;
-  // var zip = e.target.zip.value;
-  // var phoneNumber = e.target.phone_number.value;
-  // var creditCardNumber = e.target.credit_card_number.value;
-
+  new Order(item, quantity, name, street, zip, phoneNumber, creditCardNumber);
 
   //store in local storage
-  localStorage.userOrder = JSON.stringify(item);
+  localStorage.userOrder = JSON.stringify(Order.all);
 }
 
 document.getElementById('form').addEventListener('submit', handleSubmit);
