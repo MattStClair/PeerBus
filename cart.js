9f0ae7c5b38fb40099e32140d9c3392ca69081ca
mstclair <stclairm@spu.edu>
2017-07-27 15:50:14 -0700

    displays multiple images of stored items

--- cart.js
+++ cart.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var orderArray = JSON.parse(localStorage.userOrder);
-
+var container = document.getElementById('ordersContainer');
 for(var i = 0; i < orderArray.length; i++){
 
+
+  var addOrderItem = document.createElement('div');
+  var createImage = document.createElement('img');  //create variable for div
   var path = 'images/' + orderArray[i].path;
-
-  var image = document.getElementById('imageTest');
-
-  image.src = path;
+  createImage.src = path;
+  addOrderItem.appendChild(createImage); 
+  var pEl = document.createElement('p'); //create p element in that div
+  pEl.textContent = orderArray[i].item; //adds array item in local storage
+  addOrderItem.appendChild(pEl);
+  container.appendChild(addOrderItem);
 }
