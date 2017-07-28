24d720002bf149664f5b528ae38c981225488295
mstclair <stclairm@spu.edu>
2017-07-27 17:51:02 -0700

    functional! in a not so pretty way

--- cart.js
+++ cart.js
@@ -3,32 +3,36 @@
 var orderArray = JSON.parse(localStorage.userOrder);
 var container = document.getElementById('ordersContainer');
 
-var containerList = document.getElementById('userInfo');
+//var containerList = document.getElementById('userInfo');
 
 for(var i = 0; i < orderArray.length; i++){
 
+  var addOrderItem = document.createElement('div');
+
   var userNameListItem = document.createElement('p');
-  userNameListItem.textContent = 'usname: ' + orderArray[i].name;
-  container.appendChild(userNameListItem);
+  userNameListItem.textContent = 'Name: ' + orderArray[i].name;
+  addOrderItem.appendChild(userNameListItem);
 
   var userQuantLi = document.createElement('p');
-  userQuantLi.textContent = 'address: ' + orderArray[i].quantity;
-  container.appendChild(userQuantLi);
+  userQuantLi.textContent = 'Quantity: ' + orderArray[i].quantity;
+  addOrderItem.appendChild(userQuantLi);
 
   var userStreetLi = document.createElement('p');
-  userStreetLi.textContent = 'address: ' + orderArray[i].street;
-  container.appendChild(userStreetLi);
+  userStreetLi.textContent = 'Street: ' + orderArray[i].street;
+  addOrderItem.appendChild(userStreetLi);
 
   var userZipLi = document.createElement('p');
   userZipLi.textContent = 'Zipcode is: ' + orderArray[i].zip;
-  container.appendChild(userZipLi);
+  addOrderItem.appendChild(userZipLi);
 
-  var userPhoneLi = document.createElement('li');
-  userPhoneLi.textContent = 'address: ' + orderArray[i].phoneNumber;
-  container.appendChild(userPhoneLi);
+  var userPhoneLi = document.createElement('p');
+  userPhoneLi.textContent = 'Phone: ' + orderArray[i].phoneNumber;
+  addOrderItem.appendChild(userPhoneLi);
 
+  var userCardNum = document.createElement('p');
+  userCardNum.textContent = 'Card Number: ' + orderArray[i].creditCardNumber;
+  addOrderItem.appendChild(userCardNum);
 
-  var addOrderItem = document.createElement('div');
   var createImage = document.createElement('img');  //create variable for div
   var path = orderArray[i].path;
   createImage.src = path;
@@ -37,5 +41,21 @@ for(var i = 0; i < orderArray.length; i++){
   var pEl = document.createElement('p'); //create p element in that div
   pEl.textContent = orderArray[i].item; //adds array item in local storage
   addOrderItem.appendChild(pEl);
+
+
+  var but = document.createElement('button');
+  but.textContent = 'click to remove item';
+  addOrderItem.appendChild(but);
+
+  but.addEventListener('click', removeItem);
   container.appendChild(addOrderItem);
 }
+
+
+
+//
+function removeItem (event) {
+  var button = event.target;
+  button.parentNode.parentNode.removeChild(button.parentNode);
+
+ }
